9981ecfea9545c013ae678c22041d207302a018d
markush8 <markus.h8@gso.schule.koeln>
2023-10-19 13:11:14 +0200

    cancellation schema

--- api/api.py
+++ api/api.py
@@ -3,12 +3,23 @@ from flask import request   # wird benötigt, um die HTTP-Parameter abzufragen
 from flask import jsonify   # übersetzt python-dicts in json
 from marshmallow import Schema, fields
 import marshmallow
+
 class reserve_table_schema(Schema):
+    tablenumber = fields.Int(required=True)
+    number_of_guests = fields.Int(required=True)
     datetime = fields.Str(required=True)
     duration = fields.Int(required=True)
 
+class cancel_reservation_schema(Schema):
+    reservation_number = fields.Str(required=True)
+    pin = fields.Str(required=True)
+
 app = Flask(__name__)
 app.config["DEBUG"] = True  # Zeigt Fehlerinformationen im Browser, statt nur einer generischen Error-Message
+
+
+
+
 
 @app.route('/ReserveTable', methods=['POST'])
 def reserve_table():
@@ -21,13 +32,46 @@ def reserve_table():
 
     return jsonify({"message": "Table reserved successfully!"}), 201
 
+
+
+
+
+
+
+
+
+
+
+
 @app.route('/FreeTables', methods=['GET'])
 def free_tables():
     return "<h1>Freie Plaetze</h1>"
 
+
+
+
+
+
+
+
+
+
 @app.route('/CancelReservation', methods=['PUT'])
 def cancel_reservation():
-    return "<h1>Reservierung stornieren</h1>"
+    schema = cancel_reservation_schema()
+    
+    try:
+        data = schema.load(request.json)
+    except marshmallow.ValidationError as e:
+        return jsonify(e.messages), 400
+
+    return jsonify({"message": "Table reserved successfully!"}), 201
+
+
+
+
+
+
 
 @app.route('/AllReservations', methods=['GET'])
 def all_reservations():
