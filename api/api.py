5cb556c294187ffb217fda1b853eb563a4a6ed3c
Jerome Dupont <jerome.d3@gso.schule.koeln>
2023-11-23 13:16:30 +0100

    prettier caching

--- api/api.py
+++ api/api.py
@@ -3,10 +3,11 @@ import sqlite3
 from marshmallow import ValidationError
 from datetime import datetime, timedelta
 from flask import Flask, jsonify, request, Blueprint
-from flask_restful import Api, Resource
+
+# from flask_restful import Api, Resource
 from flask_cache import Cache
 
-#custom modules
+# custom modules
 from cancelRequest import CancelSchema, CancelRequest
 from freeTablesRequest import FreeTablesSchema, FreeTablesRequest
 from reserveRequest import ReserveSchema
@@ -15,45 +16,48 @@ app = Flask(__name__)
 v1_Blueprint = Blueprint(name="v1", import_name="v1")
 cache = Cache(app)
 
+
 def make_key():
-   """A function which is called to derive the key for a computed value.
-      The key in this case is the concat value of all the json request
-      parameters. Other strategy could to use any hashing function.
-   :returns: unique string for which the value should be cached.
-   """
-   user_data = request.get_json()
-   return ",".join([f"{key}={value}" for key, value in user_data.items()])
+    """A function which is called to derive the key for a computed value.
+       The key in this case is the concat value of all the json request
+       parameters. Other strategy could to use any hashing function.
+    :returns: unique string for which the value should be cached.
+    """
+    user_data = request.get_json()
+    return ",".join([f"{key}={value}" for key, value in user_data.items()])
 
 
 def init_app():
     app.config["DEBUG"] = True
-    app.register_blueprint(v1_Blueprint, url_prefix='/v1')
+    app.register_blueprint(v1_Blueprint, url_prefix="/v1")
 
-@v1_Blueprint.route('/')
+
+@v1_Blueprint.route("/")
 def home():
     app.send_static_file("/index.html")
 
+
 # ENDPOINTS
-@v1_Blueprint.route('/Reservation', methods=['POST'])
+@v1_Blueprint.route("/Reservation", methods=["POST"])
 @cache.cached(timeout=60, make_cache_key=make_key)
 def reserve_table():
     response_json = None
     data = None
-    now = datetime.now()
+    # now = datetime.now()
     pin = random.randint(1111, 9999)
-    
+
     try:
-        data = ReserveSchema().load(request.json) 
+        data = ReserveSchema().load(request.json)
     except ValidationError as e:
         return jsonify(e.messages), 400
-    
+
     try:
         con = sqlite3.connect("DB/buchungssystem.sqlite")
         con.row_factory = dict_factory
-        
-        if not is_valid_reservation_datetime(con, data.zeitpunkt):    
+
+        if not is_valid_reservation_datetime(con, data.zeitpunkt):
             return jsonify("Kann nicht in die vergangenheit buchen"), 400
-        
+
         if not has_free_table(con, data.zeitpunkt, data.tischnummer):
             return jsonify("Kein Tisch verfügbar"), 400
 
@@ -61,9 +65,11 @@ def reserve_table():
         query = "INSERT INTO reservierungen(zeitpunkt, tischnummer, pin, storniert) VALUES (?, ?, ?, ?)"
         parameters = (data.zeitpunkt, data.tischnummer, pin, "False")
         cursor.execute(query, parameters)
-        
-        response_json = get_reservation_response(con, data.zeitpunkt, data.tischnummer, pin)
-        
+
+        response_json = get_reservation_response(
+            con, data.zeitpunkt, data.tischnummer, pin
+        )
+
         con.commit()
         con.close()
     except sqlite3.Error as e:
@@ -72,7 +78,8 @@ def reserve_table():
     print(response_json)
     return response_json, 200
 
-@v1_Blueprint.route('/FreeTables', methods=['GET'])
+
+@v1_Blueprint.route("/FreeTables", methods=["GET"])
 @cache.cached(timeout=60, make_cache_key=make_key)
 def free_tables():
     try:
@@ -80,18 +87,20 @@ def free_tables():
 
         freetables_request = FreeTablesRequest(**freetables_loaded_data)
 
-        con = sqlite3.connect('./DB/buchungssystem.sqlite')
+        con = sqlite3.connect("./DB/buchungssystem.sqlite")
 
         cur = con.cursor()
         query = "SELECT * FROM reservierungen WHERE zeitpunkt LIKE '?'"
 
-        all_bookings = cur.execute(query, freetables_request.timestamp).fetchall()
+        all_bookings = cur.execute(
+            query, freetables_request.timestamp).fetchall()
         con.close()
     except ValidationError as e:
         return jsonify(e.messages), 400
     return all_bookings
 
-@v1_Blueprint.route('/Reservation', methods=['DELETE'])
+
+@v1_Blueprint.route("/Reservation", methods=["DELETE"])
 def cancel_reservation():
     try:
         cancel_loaded_data = CancelSchema().load(request.data)
@@ -109,22 +118,30 @@ def cancel_reservation():
         success = False
         for row in cursor.execute(query, cancel_request.reservation_number):
             print(row)
-            if (str(row[3]) == pin):
+            if str(row[3]) == pin:
                 success = True
 
         con.close()
     except ValidationError as e:
         return jsonify(e.messages), 400
-    if(success == False):
-        return jsonify({"message": "Cancellation not successful! Reservation number or pin not correct."}), 400
+    if not success:
+        return (
+            jsonify(
+                {
+                    "message": "Cancellation not successful! Reservation number or pin not correct."
+                }
+            ),
+            400,
+        )
     return jsonify({"message": "Cancellation successfully!"}), 201
 
-@v1_Blueprint.route('/AllReservations', methods=['GET'])
+
+@v1_Blueprint.route("/AllReservations", methods=["GET"])
 @cache.cached(timeout=60, make_cache_key=make_key)
 def all_reservations():
     try:
         con = sqlite3.connect("DB/buchungssystem.sqlite")
-        
+
         cursor = con.cursor()
         query = "SELECT * FROM reservierungen WHERE zeitpunkt > ? AND zeitpunkt < ? AND storniert=FALSE"
 
@@ -134,8 +151,9 @@ def all_reservations():
         con.close()
     except ValidationError as e:
         return jsonify(e.messages), 400
-    
+
     return result, 200
+
 
 def has_free_table(connection, zeitpunkt, tischnummer):
     cursor = connection.cursor()
@@ -144,31 +162,36 @@ def has_free_table(connection, zeitpunkt, tischnummer):
     response = cursor.execute(query, parameters)
     print(response)
     rows = response.fetchall()
-    if len(rows) > 0: 
+    if len(rows) > 0:
         return False
-    
+
     return True
+
 
 def is_valid_reservation_datetime(connection, date_time):
     now = datetime.now()
-    try: 
-        time = datetime.strptime(date_time, "%Y-%m-%d %H:%M:%S")            
-    except ValueError as e: 
+    try:
+        time = datetime.strptime(date_time, "%Y-%m-%d %H:%M:%S")
+    except ValueError:
         return False
-    if(time.minute != 0 and time.minute != 30): 
+    if time.minute != 0 and time.minute != 30:
         return False
-    if(time < now): 
+    if time < now:
         return False
     return True
 
+
 def get_reservation_response(connection, zeitpunkt, tischnummer, pin):
     cursor = connection.cursor()
-    select = "SELECT * FROM reservierungen WHERE zeitpunkt=? AND tischnummer=? AND pin=?"
+    select = (
+        "SELECT * FROM reservierungen WHERE zeitpunkt=? AND tischnummer=? AND pin=?"
+    )
     selectParams = (zeitpunkt, tischnummer, pin)
     cur_result = cursor.execute(select, selectParams)
     result = cur_result.fetchone()
-    
+
     return jsonify(result)
+
 
 def dict_factory(cursor, row):
     d = {}
@@ -176,12 +199,14 @@ def dict_factory(cursor, row):
         d[col[0]] = row[idx]
     return d
 
+
 def get_start_end_today():
     today = datetime.utcnow().date()
     start = datetime(today.year, today.month, today.day)
     end = start + timedelta(1)
-    
+
     return start, end
+
 
 init_app()
 app.run()
