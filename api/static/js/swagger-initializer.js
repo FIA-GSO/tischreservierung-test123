8cc1b3148b401e158b741350b605dc2ed56efe6e
Eduard Gottfried <eduardg14@googlemail.com>
2023-11-30 14:37:35 +0100

    fix swagger

--- api/static/js/swagger-initializer.js
+++ api/static/js/swagger-initializer.js
@@ -1,9 +1,6 @@
-window.onload = function() {
-  //<editor-fold desc="Changeable Configuration Block">
-
-  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
-  window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+indow.onload = function() {
+  const ui = SwaggerUIBundle({
+    url: document.getElementById("swagger-ui").getAttribute('openapi-url'),
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
@@ -14,7 +11,8 @@ window.onload = function() {
       SwaggerUIBundle.plugins.DownloadUrl
     ],
     layout: "StandaloneLayout"
-  });
+  })
+  // End Swagger UI call region
 
-  //</editor-fold>
+  window.ui = ui
 };
